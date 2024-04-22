48e4a97efb3a5df9d87b253ec3a127668b6b4cbc
Shouq-Mohamed <159875003+Shouq-Mohamed@users.noreply.github.com>
2024-04-22 19:37:30 +0400

    Update Part A.py

--- Part A.py
+++ Part A.py
@@ -1,7 +1,7 @@
 import random
 from enum import Enum
 import datetime
-
+import heapq
 
 class PostContent(Enum):
     NEWS = "Breaking News Update"
@@ -12,7 +12,6 @@ class PostContent(Enum):
     STORY = "Personal Story"
     QUOTE = "Inspirational Quote"
 
-
 class Post:
     def __init__(self, datetime, content, user, views):
         self.datetime = datetime
@@ -22,7 +21,6 @@ class Post:
 
     def __str__(self):
         return f"{self.datetime.strftime('%Y-%m-%d %H:%M')}: {self.content.value}, by {self.user} with {self.views} views"
-
 
 def generate_random_posts(n):
     posts = []
@@ -39,19 +37,18 @@ def generate_random_posts(n):
         posts.append(post)
     return posts
 
-
-def create_data_structures(posts):
-    post_dict = {}
-    root = None
+def create_max_heap(posts):
+    max_heap = []
     for post in posts:
-        key = post.datetime.replace(second=0, microsecond=0)
-        post_dict[key] = post
-        if root is None:
-            root = Node(post)
-        else:
-            root.insert(post)
-    return post_dict, root
+        heapq.heappush(max_heap, (-post.views, post))  # Use negative views for max heap
+    return max_heap
 
+def find_post_with_most_views(max_heap):
+    if max_heap:
+        views, post = max_heap[0]
+        return post
+    else:
+        return None
 
 def find_post_by_datetime(post_dict):
     datetime_str = input("Enter the datetime of the post (format YYYY-MM-DD HH:MM): ")
@@ -65,7 +62,6 @@ def find_post_by_datetime(post_dict):
     except ValueError:
         print("Invalid datetime format.")
 
-
 def display_all_posts(posts):
     if posts:
         for post in posts:
@@ -73,49 +69,53 @@ def display_all_posts(posts):
     else:
         print("No posts available.")
 
+def create_data_structures(posts):
+    post_dict = {}
+    max_heap = create_max_heap(posts)
+    for post in posts:
+        key = post.datetime.replace(second=0, microsecond=0)
+        post_dict[key] = post
+    return post_dict, max_heap
 
-class Node:
-    def __init__(self, post):
-        self.post = post
-        self.left = None
-        self.right = None
-
-    def insert(self, post):
-        if (post.datetime.year, post.datetime.month) < (self.post.datetime.year, self.post.datetime.month):
-            if self.left is None:
-                self.left = Node(post)
+def run_menu(post_dict, max_heap, posts):
+    while True:
+        print("\nMenu:")
+        print("1. Find post by datetime")
+        print("2. Find posts in datetime range")
+        print("3. Display all posts")
+        print("4. Display post with most views")
+        print("5. Exit")
+        choice = input("Enter your choice (1-5): ")
+        if choice == '1':
+            find_post_by_datetime(post_dict)
+        elif choice == '2':
+            find_posts_in_range(post_dict)
+        elif choice == '3':
+            display_all_posts(posts)
+        elif choice == '4':
+            post_with_most_views = find_post_with_most_views(max_heap)
+            if post_with_most_views:
+                print(f"Post with the most views: {post_with_most_views}")
             else:
-                self.left.insert(post)
+                print("No posts available.")
+        elif choice == '5':
+            print("Exiting...")
+            break
         else:
-            if self.right is None:
-                self.right = Node(post)
-            else:
-                self.right.insert(post)
+            print("Invalid choice, please enter a number between 1 and 5.")
 
-    def find_in_range(self, start_datetime, end_datetime, results):
-        if self.left is not None:
-            self.left.find_in_range(start_datetime, end_datetime, results)
-
-        if (start_datetime.year, start_datetime.month) <= (self.post.datetime.year, self.post.datetime.month) <= (
-        end_datetime.year, end_datetime.month):
-            results.append(self.post)
-
-        if self.right is not None:
-            self.right.find_in_range(start_datetime, end_datetime, results)
-
-
-def find_posts_in_range(root):
+def find_posts_in_range(post_dict):
     start_str = input("Enter the start year and month (format YYYY-MM): ")
     end_str = input("Enter the end year and month (format YYYY-MM): ")
-
     try:
         start_datetime = datetime.datetime.strptime(start_str + "-01",
                                                     "%Y-%m-%d")  # Default to the first day of the start month
         end_datetime = datetime.datetime.strptime(end_str + "-01",
                                                   "%Y-%m-%d")  # Default to the first day of the end month
-
         results = []
-        root.find_in_range(start_datetime, end_datetime, results)
+        for key, post in post_dict.items():
+            if start_datetime <= key <= end_datetime:
+                results.append(post)
         if results:
             for post in results:
                 print(post)
@@ -124,33 +124,10 @@ def find_posts_in_range(root):
     except ValueError:
         print("Invalid datetime format. Please enter the datetime in the correct format (YYYY-MM).")
 
-
-def run_menu(post_dict, root, posts):
-    while True:
-        print("\nMenu:")
-        print("1. Find post by datetime")
-        print("2. Find posts in datetime range")
-        print("3. Display all posts")
-        print("4. Exit")
-        choice = input("Enter your choice (1-4): ")
-        if choice == '1':
-            find_post_by_datetime(post_dict)
-        elif choice == '2':
-            find_posts_in_range(root)
-        elif choice == '3':
-            display_all_posts(posts)
-        elif choice == '4':
-            print("Exiting...")
-            break
-        else:
-            print("Invalid choice, please enter a number between 1 and 4.")
-
-
 def main():
     num_posts = int(input("Enter the number of posts to generate: "))
     posts = generate_random_posts(num_posts)
-    post_dict, root = create_data_structures(posts)
-    run_menu(post_dict, root, posts)
-
+    post_dict, max_heap = create_data_structures(posts)
+    run_menu(post_dict, max_heap, posts)
 
 main()
