cc81888c7b973e3c4d515a62b0e361799d8465f6
Lateeffaa <159874857+Lateeffaa@users.noreply.github.com>
2024-04-22 17:24:13 +0400

    Update Part B.py

--- Part B.py
+++ Part B.py
@@ -3,3 +3,20 @@ class Intersections:
     def __init__(self, intersectionID):
         self.intersectionID = intersectionID
 
+class Roads:
+    def __init__(self, roadID, sourceIntersect, destIntersect, roadName, length):
+        self.roadID = roadID
+        self.sourceIntersect = sourceIntersect
+        self.destIntersect = destIntersect
+        self.roadName = roadName
+        self.length = length
+
+class Houses:
+    def __init__(self, houseID):
+        self.houseID = houseID
+
+class RoadNetworks:
+    def __init__(self):
+        self.intersections = {}
+        self.roads = {}
+        self.houses = {}
